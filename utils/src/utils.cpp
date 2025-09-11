29774b3367ca9dbecb897400a97d4a3b656e2ba5
rama <amaraliaga1@gmail.com>
2025-09-11 15:08:52 +0200

    Added disparity overlay

--- utils/src/utils.cpp
+++ utils/src/utils.cpp
@@ -1,10 +1,24 @@
 #include "utils.hpp"
 
 #include <opencv2/opencv.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h> 
+#include <pcl/common/io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl/filters/voxel_grid.h>
+#include <cmath>     
+
+#include <opencv2/opencv.hpp>
 #include "stereo_calibrator.hpp"
 #include "stereo_disparity.hpp"
 #include "stereo_rectifier.hpp"
 #include "stereo_configuration.hpp"
+#include "pcd_write.hpp"
+
 #include <sl/Camera.hpp>
 #include <iostream>
 #include <vector>
@@ -12,20 +26,35 @@
 #include <filesystem>
 #include <limits>
 
-static std::vector<cv::Point> clicked_points;
-static cv::Mat global_depth_map;
+struct MouseMat {
+    cv::Mat *raw_map = nullptr;
+    cv::Mat *dis_map = nullptr;
+};
 
-// Mouse callback for measuring distance
-void onMouseMeasure(int event, int x, int y, int, void*) {
-    if (event == cv::EVENT_LBUTTONDOWN && !global_depth_map.empty()) {
-        clicked_points.push_back(cv::Point(x, y));
-        if (clicked_points.size() == 2) {
-            cv::Vec3f xyz1 = global_depth_map.at<cv::Vec3f>(clicked_points[0]);
-            cv::Vec3f xyz2 = global_depth_map.at<cv::Vec3f>(clicked_points[1]);
-            double dist = cv::norm(xyz1 - xyz2);
-            std::cout << "Distance: " << dist << " mm" << std::endl;
-            clicked_points.clear();
-        }
+typedef pcl::PointXYZRGB PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
+
+static std::vector<cv::Point> clicked_points;
+
+void onMouseMeasure(int event, int x, int y, int, void *user_data) {
+    if (event != cv::EVENT_LBUTTONDOWN) return; 
+    MouseMat *disp = static_cast<MouseMat*>(user_data);
+    if(!disp || !disp->raw_map || !disp->dis_map) return;
+
+    cv::Mat *rawMap = disp->raw_map;
+    cv::Mat *disMap = disp->dis_map;
+
+    if (x < 0 || x >= rawMap->cols || y < 0 || y >= rawMap->rows) return;
+    
+    clicked_points.push_back(cv::Point(x, y));
+    if (clicked_points.size() == 2) {
+        cv::Vec3f xyz1 = rawMap->at<float>(clicked_points[0]);
+        cv::Vec3f xyz2 = rawMap->at<float>(clicked_points[1]);
+        cv::line(*disMap, clicked_points[0], clicked_points[1], cv::Scalar(0, 0 ,255), 2);
+        cv::imshow("Disparity (vis)", *disMap);
+        double dist = cv::norm(xyz1 - xyz2);
+        std::cout << "Distance: " << dist << " mm" << std::endl;
+        clicked_points.clear();
     }
 }
 
@@ -217,6 +246,112 @@ void zed_footage() {
 }
 
 
+// bool hasField(const pcl::PCLPointCloud2& blob, const std::string& name) {
+//     for (const auto &f : blob.fields)
+//         if (f.name == name) return true;
+//     return false;
+// }
+
+// // Convert a depth map from OpenCV format to PCL point cloud.
+// // This is a direct conversion and does not require saving to disk.
+// pcl::PointCloud<pcl::PointXYZRGB>::Ptr convertCVMatToPCL(const cv::Mat& depth_map, const cv::Mat& color_image) {
+//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    
+//     // Resize the color image to match the depth map
+//     cv::Mat color_resized;
+//     cv::resize(color_image, color_resized, depth_map.size());
+
+//     for (int y = 0; y < depth_map.rows; ++y) {
+//         for (int x = 0; x < depth_map.cols; ++x) {
+//             // Get the 3D point and color from the image and depth map
+//             cv::Vec3f point = depth_map.at<cv::Vec3f>(y, x);
+//             cv::Vec3b color = color_resized.at<cv::Vec3b>(y, x);
+
+//             // Create a PCL point
+//             pcl::PointXYZRGB p;
+//             p.x = point[0];
+//             p.y = point[1];
+//             p.z = point[2];
+//             p.r = color[2];
+//             p.g = color[1];
+//             p.b = color[0];
+
+//             // If the point is valid, add it to the cloud
+//             if (p.z > 0 && p.z < 100000) { // filter out infinite depth
+//                 cloud->points.push_back(p);
+//             }
+//         }
+//     }
+//     cloud->width = static_cast<uint32_t>(cloud->points.size());
+//     cloud->height = 1;
+//     return cloud;
+// }
+
+
+// void live_disparity_map() {
+//     const std::string &s {"/home/amar-aliaga/Desktop/my_video/output.mp4"};
+
+//     StereoConfiguration config;
+
+//     if (!config.loadFromFile("config/stereo.yaml")) {
+//         std::cerr << "Error: Could not load configuration file." << std::endl;
+//         return;
+//     }
+//     StereoRectifier rectifier(config);
+//     StereoDisparity disparity_computer(config.Q);
+
+//     cv::VideoCapture cap(2);
+//     if (!cap.isOpened()) {
+//         std::cerr << "Error: Could not open video file." << std::endl;
+//         return;
+//     }
+
+//     cap.set(cv::CAP_PROP_FRAME_WIDTH, 2560);
+//     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+
+//     int w = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+//     int h = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+//     std::cout << "Res: " << w << "x" << h << std::endl;
+
+//     // --- PCL Visualizer Setup ---
+//     // -
+    
+//     // --- Main Processing Loop ---
+//     cv::Mat frame;
+//     while (true) {
+//         cap >> frame;
+//         // if (frame.empty()) {
+//         //     std::cout << "End of video." << std::endl;
+//         //     cap.set(cv::CAP_PROP_POS_FRAMES, 0); // Loop the video
+//         //     continue;
+//         // }
+
+//         cv::Mat left_raw  = frame(cv::Rect(0, 0, frame.cols / 2, frame.rows));
+//         cv::Mat right_raw = frame(cv::Rect(frame.cols / 2, 0, frame.cols / 2, frame.rows));
+        
+//         cv::Mat left_rect, right_rect;
+//         rectifier.rectify(left_raw, right_raw, left_rect, right_rect);
+        
+//         cv::Mat disp_float = disparity_computer.computeDisparity(left_rect, right_rect);
+//         cv::Mat depth_map = disparity_computer.computeDepth(disp_float);
+        
+//         // Convert to PCL point cloud in memory
+//         pcl::PointCloud<pcl::PointXYZRGB>::Ptr new_cloud = convertCVMatToPCL(depth_map, left_rect);
+//         pcl::io::savePCDFileBinary("/home/amar-aliaga/Desktop/wayland/wayland_thirdProject/results/frame_xxxxx.pcd", *new_cloud);
+//         // // Update the point cloud in the viewer
+//         //viewer.updatePointCloud<pcl::PointXYZRGB>(new_cloud, "live_cloud");
+
+//         cv::Mat res = disparity_computer.show_depthMap(depth_map);
+//         if(cv::waitKey(10) == 27) break;
+//         else {
+//             cv::imshow("depth", res);
+//         }
+//         // // Process PCL events
+//         //viewer.spinOnce(10);
+//     }
+// }
+
+
 void live_disparity_map() {
     const std::string &s {"/home/amar-aliaga/Desktop/my_video/output.mp4"};
     const std::string &v {"/home/amar-aliaga/Downloads/cam.mp4"};
@@ -252,13 +387,12 @@ void live_disparity_map() {
         }
 
 
-        cv::Mat left_raw = frame(cv::Rect(0, 0, frame.cols / 2, frame.rows));
+        cv::Mat left_raw  = frame(cv::Rect(0, 0, frame.cols / 2, frame.rows));
         cv::Mat right_raw = frame(cv::Rect(frame.cols / 2, 0, frame.cols / 2, frame.rows));
 
         
         cv::Mat left_rect, right_rect;
         rectifier.rectify(left_raw, right_raw, left_rect, right_rect);
-
         
         cv::Mat disp_float = disparity_computer.computeDisparity(left_rect, right_rect);
 
@@ -268,12 +402,23 @@ void live_disparity_map() {
 
         cv::Mat display_disparity = disparity_computer.show_disparityMap(disp_float);
         cv::Mat display_depth = disparity_computer.show_depthMap(depth_map);
+        
+
+        cv::Mat disparity_heatmap;
+        cv::applyColorMap(display_disparity, disparity_heatmap, cv::COLORMAP_JET);
+
+        cv::resize(disparity_heatmap, disparity_heatmap, left_rect.size());
+
+        cv::Mat overlay;
+        cv::addWeighted(left_rect, 0.7, disparity_heatmap, 0.3, 0, overlay);
 
 
-        cv::imshow("Left Rectified", left_raw);
-        cv::imshow("Right Rectified", right_raw);
-        cv::imshow("Disparity Map", display_disparity);
-        cv::imshow("Depth Map", display_depth);
+        // cv::imshow("Left Rectified", left_raw);
+        // cv::imshow("Right Rectified", right_raw);
+        // cv::imshow("Disparity Map", display_disparity);
+        // cv::imshow("Depth Map", display_depth);
+
+        cv::imshow("Left: rectified image + disparity overlay", overlay);
 
         if (cv::waitKey(1) == 27) {
             break;
@@ -305,38 +450,39 @@ void image_disparity_measure(const std::string &img_file) {
     cv::Mat disp_float = disparity_computer.computeDisparity(left_rect, right_rect);
     cv::Mat depth_map = disparity_computer.computeDepth(disp_float);
 
-    cv::Mat depthZ;
-    if (depth_map.channels() == 3) {
-        cv::extractChannel(depth_map, depthZ, 2);
-    } else {
-        depthZ = depth_map;
-    }
+    cv::Mat show_disp = disparity_computer.show_disparityMap(disp_float);
+    cv::Mat show_depth = disparity_computer.show_depthMap(depth_map);
 
-
-    cv::Mat z_valid = (depthZ > 0) & (depthZ < 10000) & (depthZ == depthZ);
-
-    double zmin_raw = 0, zmax_raw = 0;
-    cv::minMaxLoc(depthZ, &zmin_raw, &zmax_raw, nullptr, nullptr, z_valid);
-
-    if (!(zmax_raw > zmin_raw)) {
-        zmin_raw = 1000.0;
-        zmax_raw = 2000.0;
-    }
-
-    cv::Mat depth8u;
-    depthZ.convertTo(depth8u, CV_8U, 255.0 / (zmax_raw - zmin_raw), -255.0 * zmin_raw / (zmax_raw - zmin_raw));
-    cv::Mat depth_vis8u;
-    cv::applyColorMap(depth8u, depth_vis8u, cv::COLORMAP_TURBO);
-
-    global_depth_map = depth_map;
+    MouseMat mouse_data;
+    mouse_data.raw_map = &disp_float;
+    mouse_data.dis_map = &show_disp;
 
     cv::imshow("Rectified Left", left_rect);
     cv::imshow("Rectified Right", right_rect);
-    cv::imshow("Disparity (vis)", disp_float);
-    cv::imshow("Depth Map", depth_vis8u);
-    cv::setMouseCallback("Disparity (vis)", onMouseMeasure);
+    cv::imshow("Disparity (vis)", show_disp);
+    cv::imshow("Depth Map", show_depth);
+    cv::setMouseCallback("Disparity (vis)", onMouseMeasure, &mouse_data);
 
     std::cout << "Click two points in the Disparity (vis) window to measure distance." << std::endl;
     cv::waitKey(0);
     cv::destroyAllWindows();
+}
+
+
+void display_pointCloud() { 
+    // StereoConfiguration config;
+
+    // if (!config.loadFromFile("config/stereo.yaml")) {
+    //     return;
+    // }
+    // StereoRectifier rectifier(config);
+
+    // StereoDisparity disparity_computer(config.Q);
+
+    // cv::Mat disp_float = disparity_computer.computeDisparity(left_rect, right_rect);
+    // cv::Mat depth_map = disparity_computer.computeDepth(disp_float);
+
+    // PointCloudT::Ptr cloud2 = convertCVMatToPCL(depth_map);
+    // pcl::io::savePCDFileBinary("results/frame_xxxxx.pcd", *cloud2);
+    std::cout << "Hello" << std::endl;
 }
